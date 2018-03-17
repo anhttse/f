3271889752fe277581396077f9965cab2fa5475f
anhttse <zlegendzz@gmail.com>
2018-03-18 00:42:09 +0700

    update
    update

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -10,7 +10,48 @@ chrome.tabs.query({active:true,currentWindow:true},function(tabs){
     });
 });
 
-$('#token').on('change',function(e){
-    // e.prevenDefault();
-    $("#total_count").html("up");
-})
+// $('#token').on('change',function(e){
+//     // e.prevenDefault();
+//     $("#total_count").html("up");
+// })
+// var ids;
+// window.onload = function() {
+
+//     var fileInput = document.getElementById('fileinput');
+//     var fileDisplayArea = document.getElementById('total_count');
+//     fileInput.addEventListener('change', function(e) {
+        
+//         var file = fileInput.files[0];
+//         var textType = /text.*/;
+
+//         if (file.type.match(textType)) {
+//             var reader = new FileReader();
+
+//             reader.onload = function(e) {
+//                 ids = reader.result.split('|');
+//                 ids.pop();
+//                 console.log(ids);
+//             }
+
+//             reader.readAsText(file);	
+//         } else {
+//             fileDisplayArea.innerText = "File not supported!"
+//         }
+//     });
+// }
+
+$('#btnBrowse').on('click',()=>{
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,{btn:"generate"},function(res){
+            
+        });
+    });
+});
+
+$('#unf').on('click',()=>{
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,{btn:"unf"},function(res){
+            
+        });
+    });
+});
